0e7c0b7ab8adb3a9a6d4595f0dd5e2a0755a4653
wmjolly <wmjolly@gmail.com>
2025-01-15 12:30:23 -0700

    Updating Docs and Sphinx conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-breathe_projects = {
-"nfdrs": "./_build/xml/",
-}
+breathe_projects = {"nfdrs4": "_build/xml/"}
 
 # Breathe configuration
 breathe_default_project = "nfdrs4"
